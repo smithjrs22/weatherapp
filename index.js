00de9a457fddfe1dce04d6b128256231e988dbc3
9CD1KC2\admin <smithjrs22@gmail.com>
2022-04-08 13:40:49 -0400

    more changes

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 //store object for using api//
+
 let weather = {
     apiKey: "eee684649ca8f26e54eba6e99b817178",
-    fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&appid=eee684649ca8f26e54eba6e99b817178")
-        .then((reponse) => response.json()) 
-        .then((data) => console.log(data));
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial&appid=" + this.apiKey
+        )
+        .then((response) => response.json()) 
+        .then((data) => console.log(this.displayWeather(data));
     },
+    displayWeather: function(data) {
+        let {name} = data;
+        let {icon, description} = data.weather[0];
+        let {temp, humidity} = data.main;
+        let {speed} = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = "Temp:" + temp + "°F"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity for today is" + humidity + "%"
+        document.querySelector(".icon").src = 
+        "" + icon + ".png"
+        
+    }
 };
      
    
